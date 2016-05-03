a1c5dcb2fe84da869b6ee6a1cd67d383d67b1e9e
Christian Ceciliano <cristiancehi@gmail.com>
2016-05-03 03:32:10 -0500

    Fix: Memory limit for background process

--- worker/background.js
+++ worker/background.js
@@ -3,18 +3,23 @@
 var {app, redis, api} = require('./.');
 
 module.exports = function() {
-  return redis.srandmember('summoners')
-  .then(function(value) {
-    if (!value) {
-      console.log('No hay datos con los que trabajar.');
-      return;
+  return redis.info('memory').then(function(res) {
+    var memory = Number(res.split('\n')[1].split(':')[1]);
+    if (memory < (app.kraken.get('redisMemory') || 32) * 1024 * 1024) {
+      redis.srandmember('summoners')
+      .then(function(value) {
+        if (!value) {
+          console.log('No hay datos con los que trabajar.');
+          return;
+        }
+        console.log('Re-añadiendo elementos a la lista.');
+        return redis.sadd('pending:summoners', value).then(function() {
+          return require('./pendingSummoners')();
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
     }
-    console.log('Re-añadiendo elementos a la lista.');
-    return redis.sadd('pending:summoners', value).then(function() {
-      return require('./pendingSummoners')();
-    });
-  })
-  .catch(function(err) {
-    console.error(err);
   });
 };
